ab2891b906feb8a3f586f46e7f8367e9a6d1ef2c
User Synax <usersynax@gmail.com>
2026-03-23 10:04:57 +0530

    chore: export all components from ui index

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,7 @@
+export * from "./button";
+export * from "./input";
+export * from "./card";
+export * from "./modal";
+export * from "./badge";
+export * from "./avatar";
+export * from "./dropdown";
